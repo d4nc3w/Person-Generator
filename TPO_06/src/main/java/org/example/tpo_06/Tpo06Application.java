2eac786a87ddb7bb8b8b832476c9375dbf16f730
s28113 <s28113@pjwstk.edu.pl>
2024-04-25 22:19:57 +0200

    Deleted commented code

--- TPO_06/src/main/java/org/example/tpo_06/Tpo06Application.java
+++ TPO_06/src/main/java/org/example/tpo_06/Tpo06Application.java
@@ -11,16 +11,6 @@ public class Tpo06Application {
     public static void main(String[] args) {
         SpringApplication.run(Tpo06Application.class, args);
 
-       /*
-       String[] countryCodes = Locale.getISOCountries();
-
-        for (String countryCode : countryCodes) {
-
-            Locale obj = new Locale("", countryCode);
-
-            System.out.println("Country Code = " + obj.getCountry()
-                    + ", Country Name = " + obj.getDisplayCountry());
-        }*/
     }
 
 }
